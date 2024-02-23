3dc46a5db8bccb756caeb3736de24b7cbf9032f5
khushishah-15 <khushis@zignuts.com>
2024-02-23 11:45:34 +0530

    login signup working--added bcrypt

--- Hotel management system/app.js
+++ Hotel management system/app.js
@@ -7,11 +7,13 @@ const app = express();
 const port = 5000;
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded( { extended : false} ) );
+
 app.use(express.json());
+app.use(express.urlencoded( { extended : false} ) );
 
 //static file
 app.use(express.static("public"));
+
 app.get( "/", (req, res) =>{
     res.render("login");
 });
@@ -27,8 +29,43 @@ app.post("/signup", async (req,res) => {
       password: req.body.password
      }
 
-     const userdata = await collection.insertMany(data);
-     console.log(userdata);
+     const existingUser = await collection.findOne({name: data.name}); 
+     if(existingUser){
+        res.send ("user already exist! please choose a different username");
+     }else {
+        //hash the password uding bcrypt
+        const  saltRounds = 10;
+        let hashedPassword= await bcrypt.hash(data.password ,saltRounds);
+        data.password = hashedPassword; //Replace the hashed password with oiginal password
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+     } 
+});
+
+app.post("/login", async (req,res) => {
+    try{
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+          return res.send("Please enter both username and password!");
+        }
+        
+        const user = await collection.findOne({ name: username }); 
+        if(!user){
+          res.send ("username dosn't exist.check username or Signup");
+        }
+
+        //compare the hash password from the database with plaintext
+        const isPasswordMatch= await bcrypt.compare(password ,user.password);
+        if(isPasswordMatch) {
+          res.render("home")
+        }else{
+          res.send("wrong password!");
+        }
+      } catch (error) {
+        console.error(error);
+        res.send("Something went wrong!");
+      }        
 });
 
   app.listen(port, () => {
--- Hotel management system/src/config.js
+++ Hotel management system/src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017")
+const connect = mongoose.connect("mongodb://localhost:27017/login")
  connect.then(() => {
   console.log("Database connected successfully");
   }).catch((err) => {
